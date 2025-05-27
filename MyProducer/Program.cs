4461bf249a9e05d37c57ed74cb829ec8bb97bf5a
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2025-05-27 13:10:01 +0300

    Competing consumers pattern. Only one consumer processes an event

---
+++ MyProducer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "askrwin10.kansys.local" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+const string queueName = "myQueue";
+
+await channel.QueueDeclareAsync(
+    queue: queueName,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+for (var i = 0; i < 10; i++)
+{
+    var message = $"{DateTime.UtcNow} {Guid.CreateVersion7()}";
+    var body = Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: queueName,
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body);
+    
+    Console.WriteLine($"Sent: {message}");
+    await Task.Delay(2000);
+}
